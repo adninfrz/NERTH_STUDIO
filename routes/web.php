5d992e7e3842f157ec1911d0798e996d33ca9ebf
Zachran Razendra <razezach15@gmail.com>
2024-07-29 01:51:41 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index'])->name('home');
 Route::get('About', [MainController::class, 'about'])->name('about');
 Route::get('/product', [MainController::class, 'product'])->name('product');
+
+Route::get('/auth/create-account', [AuthController::class, 'create'])->name('create.account');
